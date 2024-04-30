63b47c81d1b1063bb316c58110027614462b4dbd
IgorChijov <igorchijof@gmail.com>
2024-04-30 23:44:06 +0300

    условные операторыы - 2(корректировка)

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,13 @@ public class Main {
         }
 
         int clientDeviceYear = 2015;
-        if (clientOS == 0 && clientDeviceYear <2015) {
+        if (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для IOS по ссылке");
-        } else if(clientOS == 0 && clientDeviceYear >= 2015) {
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию для IOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear < 2015){
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию для Android по ссылке");
-        }   else if (clientOS == 1 && clientDeviceYear >=2015){
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию для Android по ссылке");
         }
 
@@ -31,26 +31,24 @@ public class Main {
         int deliveryTime = 1;
         if (deliveryDistance > 100) {
             System.out.println("Доставки нет");
-        } else{
+        } else {
             if (deliveryDistance > 20) {
-                deliveryDistance++;
+                deliveryTime++;
             }
             if (deliveryDistance > 60) {
-                deliveryDistance++;
+                deliveryTime++;
             }
-            System.out.println("Потребуется дней " + deliveryDistance);
+            System.out.println("Потребуется дней " + deliveryTime);
         }
-
-
 
 
         int monthNumber = 6;
         switch (monthNumber) {
-            case 12,1,2 -> System.out.println("Зима");
-            case 3,4,5 -> System.out.println("Весна");
-            case 6,7,8 ->System.out.println("Лето");
-            case 9,10,11 -> System.out.println("Осень");
-            default -> System.out.println("Номер месяца больше 12..");
+            case 12, 1, 2 -> System.out.println("Зима");
+            case 3, 4, 5 -> System.out.println("Весна");
+            case 6, 7, 8 -> System.out.println("Лето");
+            case 9, 10, 11 -> System.out.println("Осень");
+            default -> System.out.println("Некорректный номер месяца");
         }
     }
 }
